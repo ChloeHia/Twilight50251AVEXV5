1c2aa1dd02201ca5cb8ff2c0932edd78b7803459
ChloeHia <chloegwynnethhia@gmail.com>
2026-03-25 10:44:17 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -27,10 +27,22 @@ competition Competition;
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
-
+void calibrateinertialsensor() {
+  Inertialsensor.calibrate();
+    while (Inertialsensor.isCalibrating()) {
+      Controller1.Screen.clearLine(3);
+      Controller1.Screen.print("Calibrating");
+    }
+      Controller1.Screen.clearLine(3);
+      Controller1.Screen.print("Done");
+      Controller1.rumble("-.-");
+      wait (2, msec);
+}
 
 void pre_auton(void) {
 
+    vexcodeinit();
+  calibrateinertialsensor();
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
@@ -61,11 +73,122 @@ void autonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+   void Armup() {
+      PneumaticsA.close();
+    }
+
+    void Armdown() {
+      PneumaticsA.open();
+    }
+
+    void setPosition(
+    double value,rotationUnits units );
+
+    void movemotora() {
+    MotorA.spin(forward);
+    }
+
+    void movemotorb() {
+    MotorB.spin(forward);
+    }
+
+    void movemotorc() {
+    MotorC.spin(forward);
+    }
+
+    void movemotord() {
+    MotorD.spin(forward);
+    }
+
+    void liftingtube() {
+      PneumaticsB.close();
+    }
+
+    void descendingtube() {
+      PneumaticsB.open();
+    }
+
+    void middlecontrol() {
+      PneumaticsC.close();
+    }
+
+    void topcontrol() {
+      PneumaticsC.open();
+    }
+
+    void stopmotorabcd() {
+      MotorA.stop();
+      MotorB.stop();
+      MotorC.stop();
+      MotorD.stop();
+    }
+
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
     // Inside usercontrol loop
-    drivetrainright.spin(forward, Controller1.Axis3.position(), percent);
+
+    //drivetrains forward and backward
+    if (Controller1.Axis3.position() > 10) {
+        drivetrainleft.spin(forward);
+        drivetrainright.spin(forward);
+    } else if (Controller1.Axis3.position() < -10) {
+        drivetrainleft.spin(reverse);
+        drivetrainright.spin(reverse);
+    } else {
+        drivetrainleft.stop();
+        drivetrainright.stop();
+    }
+
+        //Drivetrain will be solved later problem
+
+    //drivetrain left and right
+    if (Controller1.Axis4.position() > 10) {
+        drivetrainleft.spin(forward);
+        drivetrainright.spin(reverse);
+    } else if (Controller1.Axis4.position() < -10) {
+        drivetrainleft.spin(reverse);
+        drivetrainright.spin(forward);
+    } else {
+        drivetrainleft.stop();
+        drivetrainright.stop();
+    }
+    
+
+    //Pneumatic arms
+    Controller1.ButtonL1.pressed(Armdown);
+    Controller1.ButtonL2.pressed(Armup);
+
+    //pneumatic switching between top & middle control zones
+    Controller1.ButtonUp.pressed(topcontrol);
+    Controller1.ButtonLeft.pressed(middlecontrol);
+
+    //pneumatic lifting and descending of the mechanism for the tube balls
+    Controller1.ButtonDown.pressed(descendingtube);
+    Controller1.ButtonRight.pressed(liftingtube);
+
+    //4 motors (a, b, c, d)
+    Controller1.ButtonA.pressed(movemotora);
+    Controller1.ButtonX.pressed(movemotorb);
+    Controller1.ButtonY.pressed(movemotorc);
+    Controller1.ButtonB.pressed(movemotord);
+    Controller1.ButtonR1.pressed(stopmotorabcd);
+
+    //rotation sensor
+    double angleX = RotationX.angle(degrees);
+    double angleY = RotationY.angle(degrees);
+    RotationX.setPosition(0.0, degrees);
+    RotationY.setPosition(0.0, degrees);
+    
+    // Display a message at the starting cursor
+    Brain.Screen.setCursor(3, 12);
+    Brain.Screen.print("Angle X value:");
+    Brain.Screen.setCursor(3, 30);
+    Brain.Screen.print(angleX);
+    Brain.Screen.setCursor(5, 12);
+    Brain.Screen.print("Angle Y value:");
+    Brain.Screen.setCursor(5, 30);
+    Brain.Screen.print(angleY);
 
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -6,15 +6,40 @@ using namespace vex;
 brain Brain;
 
 controller Controller1 = controller(primary);
-motor FL = motor(PORT1, ratio6_1, false); //port number, gear_ratio, reverse = true
-motor ML = motor(PORT2, ratio6_1, false); //f= front, m= middle, b= back
-motor BL = motor(PORT3, ratio6_1, false); //r= right, l= left
-motor FR = motor(PORT4, ratio18_1, false); //modify port number, ratio, reverse
-motor MR = motor(PORT5, ratio18_1, false); 
-motor BR = motor(PORT6, ratio18_1, false); 
 
+//drivetrain motors
+motor FL = motor(PORT1, ratio6_1, false); //port number, gear_ratio, reverse = true
+motor ML = motor(PORT2, ratio6_1, true); //f= front, m= middle, b= back
+motor BL = motor(PORT3, ratio6_1, false); //r= right, l= left
+motor FR = motor(PORT4, ratio6_1, false); //modify port number, ratio, reverse
+motor MR = motor(PORT5, ratio6_1, true); 
+motor BR = motor(PORT6, ratio6_1, false); 
+
+//drivetrain group
 motor_group drivetrainright = motor_group(FR, MR, BR);  //Drivetrain Motor Right
 motor_group drivetrainleft = motor_group(FL, ML, BL); //Drivetrain Motor Left
+
+//pneumatics arm
+pneumatics PneumaticsA = pneumatics(Brain.ThreeWirePort.A);
+
+//lifting and descending of the mechanism pneumatics tube balls 
+pneumatics PneumaticsB = pneumatics(Brain.ThreeWirePort.B);
+
+//switching between top & middle control zones
+pneumatics PneumaticsC = pneumatics(Brain.ThreeWirePort.C);
+
+//4 of the motors
+motor MotorA = motor(PORT9, false);
+motor MotorB = motor(PORT10, false);
+motor MotorC = motor(PORT11, false);
+motor MotorD = motor(PORT12, false);
+
+//Rotation sensors
+rotation RotationX = rotation(PORT7);
+rotation RotationY = rotation(PORT8);
+
+//Inertial sensors
+inertial Inertialsensor = inertial(PORT13); 
 
 void vexcodeinit(void) {
 }
